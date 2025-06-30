488207a2ca93aab3af36d8b157972ff21f12efba
Gilbert <gillyronex@gmail.com>
2025-06-30 15:20:22 +0300

    update

--- housemanage/models.py
+++ housemanage/models.py
@@ -7,3 +7,16 @@ class Owner(models.Model):
     email=models.EmailField(unique=True)
     phone=models.PositiveBigIntegerField()
 
+class House (models.Model):
+    address=models.CharField(max_length=100)
+    Owner=models.ForeignKey(Owner, on on_delete==models.)
+    num_rooms=models.PositiveIntegerField()
+    is_available=models.BooleanField(default=True)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+
+class Tenant(models.model)
+    name=models.CharField(max_length=30)
+    email=models.EmailField(unique=True)
+    phone=models.PositiveBigIntegerField()
+    house=models.ForeignKey(House, on_delete=models.CASCADE)
+    
